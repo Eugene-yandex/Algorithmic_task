1a8bc97ed54699aee24a107f799f13d7ccb253f4
Eugene-yandex <154636169+Eugene-yandex@users.noreply.github.com>
2024-11-06 15:22:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <bitset>
 
-enum class ANSER {
-	N = 0b1000,
-	S = 0b0100,
-	W = 0b0010,
-	E = 0b0001,
-	NW = 0b1010,
-	NE = 0b1001,
-	SW = 0b0110,
-	SE = 0b0101
-};
 
 int main() {
 	std::vector<int> coordinats;
@@ -24,52 +13,17 @@ int main() {
 		coordinats[i] = std::stoi(value);
 	}
 
-	bool itog[4]{ false };
 	if (coordinats[3] < coordinats[5]) {
-		itog[0] = true;
+		std::cout << 'N';
 	}
 	if (coordinats[1] > coordinats[5]) {
-		itog[1] = true;
+		std::cout << 'S';
 	}
 	if (coordinats[0] > coordinats[4]) {
-		itog[2] = true;
+		std::cout << 'W';
 	}
 	if (coordinats[2] < coordinats[4]) {
-		itog[3] = true;
-	}
-	std::bitset<8> bit;
-	for (int i = 3; i >= 0; --i) {
-		bit.set(i, itog[3-i]);
-	}
-	ANSER res = static_cast<ANSER>(bit.to_ulong());
-	switch (res) {
-	case ANSER::E:
 		std::cout << 'E';
-		break;
-	case ANSER::N:
-		std::cout << 'N';
-		break;	
-	case ANSER::NE:
-		std::cout << "NE";
-			break;
-	case ANSER::NW:
-		std::cout << "NW";
-		break;
-	case ANSER::S:
-		std::cout << 'S';
-		break;
-	case ANSER::SE:
-		std::cout << "SE";
-		break;
-	case ANSER::SW:
-		std::cout << "SW";
-		break;
-	case ANSER::W:
-		std::cout << 'W';
-		break;
-
-	default:
-		break;
 	}
 
 }
